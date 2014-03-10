a3c983dc68e2872f901e2dde795fe2c8474d7bbd
Leeds Data Mill <mark@leedsdatamill.org>
2014-03-10 14:27:32 +0000

    Increased the image size of the featured image on upload

--- leedsdatamill/functions.php
+++ leedsdatamill/functions.php
@@ -67,7 +67,7 @@ function ip_wpcf7_mail_sent($wpcf7)
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'wpbs-featured', 780, 300, true );
+add_image_size( 'wpbs-featured', 1100, 423, true );
 add_image_size( 'wpbs-featured-home', 970, 311, true);
 add_image_size( 'wpbs-featured-carousel', 970, 400, true);
 
